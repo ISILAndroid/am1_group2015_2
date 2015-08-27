3c075537ab4a2e6db474924fc620250ebf073b9d
Eduardo Medina <em@osp.pe>
2015-08-27 10:28:34 -0500

    ejemplos java

---
+++ ExamplesJava/app/src/main/java/com/isil/examplesjava/model/IPersona.java
@@ -0,0 +1,12 @@
+package com.isil.examplesjava.model;
+
+/**
+ * Created by emedinaa on 27/08/15.
+ */
+public interface IPersona {
+
+    void jugar();
+    void comer();
+    void dormir();
+    void programar();
+}
