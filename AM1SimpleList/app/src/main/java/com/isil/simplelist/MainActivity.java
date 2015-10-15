8c3d33a02024fae50c97d206d460d89f6a6e6b53
emedinaa <emedinaa@gmail.com>
2015-10-15 08:04:28 -0500

    simple list

---
+++ AM1SimpleList/app/src/main/java/com/isil/simplelist/MainActivity.java
@@ -0,0 +1,55 @@
+package com.isil.simplelist;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private String[] mStrings = Cheeses.sCheeseStrings;
+    private ListView lviCheeses;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lviCheeses=(ListView)findViewById(R.id.lviCheeses);
+        lviCheeses.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mStrings));
+        lviCheeses.setTextFilterEnabled(true);
+
+        //events
+        lviCheeses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String value= String.valueOf(adapterView.getAdapter().getItem(position));
+                showItem(value);
+            }
+        });
+    }
+
+    private void showItem(String value) {
+
+        Toast.makeText(this,"item "+value,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        return false;
+    }
+}
